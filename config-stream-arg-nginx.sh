f8fca85461995486ab9d93901a8af1dd7681c0db
Antony Ho <ntonyworkshop@gmail.com>
2017-01-23 02:23:32 +0800

    Fix shell script problem

--- config-stream-arg-nginx.sh
+++ config-stream-arg-nginx.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sed -i -- 's/TWITCH_ADDRESS/$TWITCH_ADDRESS/g' /etc/nginx/nginx.conf
-sed -i -- 's/TWITCH_STREAM_KEY/$TWITCH_STREAM_KEY/g' /etc/nginx/nginx.conf
-sed -i -- 's/YOUTUBE_ADDRESS/$YOUTUBE_ADDRESS/g' /etc/nginx/nginx.conf
-sed -i -- 's/YOUTUBE_STREAM_KEY/$YOUTUBE_STREAM_KEY/g' /etc/nginx/nginx.conf
+sed -i -- "s/TWITCH_ADDRESS/$TWITCH_ADDRESS/g" /etc/nginx/nginx.conf
+sed -i -- "s/TWITCH_STREAM_KEY/$TWITCH_STREAM_KEY/g" /etc/nginx/nginx.conf
+sed -i -- "s/YOUTUBE_ADDRESS/$YOUTUBE_ADDRESS/g" /etc/nginx/nginx.conf
+sed -i -- "s/YOUTUBE_STREAM_KEY/$YOUTUBE_STREAM_KEY/g" /etc/nginx/nginx.conf
 
 service nginx start
