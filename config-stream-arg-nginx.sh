53cab88d979549185852464fceb770b6ac1f162b
Antony Ho <ntonyworkshop@gmail.com>
2017-01-23 02:55:13 +0800

    Forgot to replace the original file

--- config-stream-arg-nginx.sh
+++ config-stream-arg-nginx.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sed "s|TWITCH_ADDRESS|$TWITCH_ADDRESS|g" /etc/nginx/nginx.conf
-sed "s|TWITCH_STREAM_KEY|$TWITCH_STREAM_KEY|g" /etc/nginx/nginx.conf
-sed "s|YOUTUBE_ADDRESS|$YOUTUBE_ADDRESS|g" /etc/nginx/nginx.conf
-sed "s|YOUTUBE_STREAM_KEY|$YOUTUBE_STREAM_KEY|g" /etc/nginx/nginx.conf
+sed "s|TWITCH_ADDRESS|$TWITCH_ADDRESS|g" -i /etc/nginx/nginx.conf
+sed "s|TWITCH_STREAM_KEY|$TWITCH_STREAM_KEY|g" -i /etc/nginx/nginx.conf
+sed "s|YOUTUBE_ADDRESS|$YOUTUBE_ADDRESS|g" -i /etc/nginx/nginx.conf
+sed "s|YOUTUBE_STREAM_KEY|$YOUTUBE_STREAM_KEY|g" -i /etc/nginx/nginx.conf
 
 service nginx start
