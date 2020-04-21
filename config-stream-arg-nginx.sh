a322b0a1fa1fa23b4226c810099fe98ddaf0abb2
Antony Ho <ntonyworkshop@gmail.com>
2020-04-21 17:52:55 +0200

    Fix the line to add push in config file

--- config-stream-arg-nginx.sh
+++ config-stream-arg-nginx.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 if [[ ! -z "$MIXER_ADDRESS" ]]; then
-	sed -i "16i\\            push rtmp://$MIXER_ADDRESS/$MIXER_STREAM_KEY;" /root/nginx-rmtp-cfg-simple.txt
+	sed -i "14i\\            push rtmp://$MIXER_ADDRESS/$MIXER_STREAM_KEY;" /root/nginx-rmtp-cfg-simple.txt
 fi
 
 if [[ ! -z "$FACEBOOK_ADDRESS" ]]; then
-	sed -i "16i\\            push rtmp://$FACEBOOK_ADDRESS/$FACEBOOK_ADDRESS;" /root/nginx-rmtp-cfg-simple.txt
+	sed -i "14i\\            push rtmp://$FACEBOOK_ADDRESS/$FACEBOOK_ADDRESS;" /root/nginx-rmtp-cfg-simple.txt
 fi
 
 if [[ ! -z "$TWITCH_ADDRESS" ]]; then
-	sed -i "16i\\            push rtmp://$TWITCH_ADDRESS/$TWITCH_STREAM_KEY;" /root/nginx-rmtp-cfg-simple.txt
+	sed -i "14i\\            push rtmp://$TWITCH_ADDRESS/$TWITCH_STREAM_KEY;" /root/nginx-rmtp-cfg-simple.txt
 fi
 
 if [[ ! -z "$YOUTUBE_ADDRESS" ]]; then
-	sed -i "16i\\            push rtmp://$YOUTUBE_ADDRESS/$YOUTUBE_STREAM_KEY;" /root/nginx-rmtp-cfg-simple.txt
+	sed -i "14i\\            push rtmp://$YOUTUBE_ADDRESS/$YOUTUBE_STREAM_KEY;" /root/nginx-rmtp-cfg-simple.txt
 fi
 
 cp /root/nginx-rmtp-cfg-simple.txt /etc/nginx/nginx.conf
